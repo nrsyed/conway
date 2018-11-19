81f97c74f8f9c87eecfad5e745ed130ce173202a
Najam <najam.r.syed@gmail.com>
2018-11-19 13:00:00 -0500

    Add function to compute neighborhood cell kernel

--- javascript/conway.js
+++ javascript/conway.js
@@ -1,6 +1,6 @@
 var canvas, ctx, cellWidth, cellHeight;
 var grid, newGrid;
-var rows, cols;
+var numRows, numCols;
 var colors = ['rgb(240, 240, 240)', 'rgb(10, 10, 10)'];
 
 function init() {
@@ -8,13 +8,15 @@ function init() {
   ctx = canvas.getContext('2d');
   canvas.width = 800;
   canvas.height = 800;
-  rows = 5;
-  cols = 5;
+  numRows = 5;
+  numCols = 5;
 
-  cellWidth = canvas.width / cols;
-  cellHeight = canvas.height / rows;
+  cellWidth = canvas.width / numCols;
+  cellHeight = canvas.height / numRows;
 
-  grid = Array.from(Array(rows), () => Array(cols).fill(0));
+  grid = Array.from(Array(numRows), () => Array(numCols).fill(0));
+  grid[1][1] = 1;
+  grid[2][2] = 1;
 
   document.getElementById('canvas-width').innerHTML = canvas.width;
   document.getElementById('canvas-height').innerHTML = canvas.height;
@@ -30,11 +32,23 @@ function sleep(ms) {
 }
 
 function drawGrid() {
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
+  for (let i = 0; i < numRows; i++) {
+    for (let j = 0; j < numCols; j++) {
       setCellColor(i, j, colors[grid[i][j]]);
     }
   }
+}
+
+function neighborSum(row, col) {
+  let sum = -grid[row][col];
+  for (let i of [-1, 0, 1]) {
+    for (let j of [-1, 0, 1]) {
+      if (row + i >= 0 && row + i < numRows && col + j >= 0 && col + j < numCols) {
+        sum += grid[row + i][col + j];
+      }
+    }
+  }
+  return sum;
 }
 
 init();
